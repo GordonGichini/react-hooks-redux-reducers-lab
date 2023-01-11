f5d1a3b0b95d2e4e670cc91a5ccb7e2df73fa679
GordonGichini <gordongichini1@gmail.com>
2023-01-11 05:10:00 +0300

    reducer functions added

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,13 @@
-export function manageFriends(state, action) {
+export function manageFriends(state = {friends: []}, action) {
   // your code here
+  switch (action.type) {
+    case "friends/add":
+      return {...state, friends: [...state.friends,action.payload]};
+      case "friends/remove":
+        return {...state, friends: state.friends.filter(function (friend) {
+            return friend.id !== action.payload;
+          })};
+          default:
+            return state;
+  } 
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,15 @@
 const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
-  // your code here
+  // your code here 
+  switch(action.type) {
+    case "presents/increase":
+    return {...state, numberOfPresents:
+      state.numberOfPresents + 1};
+    case "presents/decrease":
+      return {...state, numberOfPresents:
+      state.numberOfPresents - 1};
+      default:
+        return state;
+  }
 }
